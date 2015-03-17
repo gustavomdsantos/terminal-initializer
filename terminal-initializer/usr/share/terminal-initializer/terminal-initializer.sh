ea9730fb091d4fbf38dd0b01a51c0daa3a520e88
gustavosotnas <gustavo_moraiss@hotmail.com>
2015-03-17 11:47:27 -0300

    Lançamento da versão 0.1.0 Otimizados scripts de configuração da instalação do aplicativo. Adicionado campos de Autor e informações sobre licença no cabeçalho de todos os códigos-fonte do projeto.

--- terminal-initializer/usr/share/terminal-initializer/terminal-initializer.sh
+++ terminal-initializer/usr/share/terminal-initializer/terminal-initializer.sh
@@ -6,17 +6,21 @@
 # License. See the file COPYING in the main directory of this archive
 # for more details.
 
+APP_NAME="Terminal Initializer"
+VERSION="0.1.0"
+APP_AUTHOR="Copyright (C) 2014-2015 Gustavo Moraes http://about.me/gustavosotnas"
+
 	# ls → lista os arquivos do diretório, um arquivo por linha e wc -l → conta a qtd de linhas
-	QTD_LINHAS=$(ls -1 $HOME/terminalInitializer/files/ | wc -l);
+	QTD_LINHAS=$(ls -1 $HOME/terminal-initializer/files/ | wc -l);
 
 	# sorteia um número aleatório limitado à quantidade de arquivos do diretório de dicas
 	LINHA=$[RANDOM%$QTD_LINHAS+1];
 
 	# lista novamente os arquivos, e recupera o nome do arquivo na linha sorteada
-	ARQ=$(ls -1 $HOME/terminalInitializer/files/ | tail -n $LINHA | head -n 1);
+	ARQ=$(ls -1 $HOME/terminal-initializer/files/ | tail -n $LINHA | head -n 1);
 
 	# lê o arquivo da dica
-	TEXTO="`cat "$HOME/terminalInitializer/files/$ARQ"`";
+	TEXTO="`cat "$HOME/terminal-initializer/files/$ARQ"`";
 
 	# obtem as informacoes do nome da distribuição e do kernel Linux
 	DISTRIB_NOME=$(lsb_release -sd);
